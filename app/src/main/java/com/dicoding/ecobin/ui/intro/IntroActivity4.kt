1646081651a6b486ae6aad6dd1881eb1f3fadb97
lingga24 <lingga.dian.lestari@gmail.com>
2023-12-05 23:25:10 +0700

    add login page

--- app/src/main/java/com/dicoding/ecobin/ui/intro/IntroActivity4.kt
+++ app/src/main/java/com/dicoding/ecobin/ui/intro/IntroActivity4.kt
@@ -1,14 +1,20 @@
 package com.dicoding.ecobin.ui.intro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.ecobin.R
 import com.dicoding.ecobin.databinding.ActivityIntro4Binding
+import com.dicoding.ecobin.ui.LoginActivity
 
 class IntroActivity4 : AppCompatActivity() {
     private lateinit var binding: ActivityIntro4Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro4)
+        binding = ActivityIntro4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
