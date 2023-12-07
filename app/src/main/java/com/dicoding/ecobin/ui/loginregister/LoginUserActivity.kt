f350424db19eecb00e6f7657d6977a624925e5d8
lingga24 <lingga.dian.lestari@gmail.com>
2023-12-07 11:03:04 +0700

    dashboard page

--- app/src/main/java/com/dicoding/ecobin/ui/loginregister/LoginUserActivity.kt
+++ app/src/main/java/com/dicoding/ecobin/ui/loginregister/LoginUserActivity.kt
@@ -1,12 +1,20 @@
 package com.dicoding.ecobin.ui.loginregister
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.ecobin.R
+import com.dicoding.ecobin.databinding.ActivityLoginUserBinding
+import com.dicoding.ecobin.ui.Dashboard
 
 class LoginUserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_user)
+        binding = ActivityLoginUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, Dashboard::class.java))
+        }
     }
 }
