acaf693e41c5db2f5d32138d307dad5bfeecfb66
lingga24 <lingga.dian.lestari@gmail.com>
2023-12-11 10:51:03 +0700

    user preference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,12 +38,13 @@ android {
 }
 
 dependencies {
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.google.android.gms:play-services-auth:20.6.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.datastore:datastore-core:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/dicoding/ecobin/pref/UserModel.kt
@@ -0,0 +1,5 @@
+package com.dicoding.ecobin.pref
+data class UserModel(
+    val email: String,
+    val isLogin: Boolean = false
+)
